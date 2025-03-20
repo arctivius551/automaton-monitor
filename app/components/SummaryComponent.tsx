2eb5e2e14f43f5ac18ead069522e5f56a2a264eb
Ryan Bucinell <rbucinell.dev@gmail.com>
2025-03-19 22:55:55 -0400

    adding some icons

--- app/components/SummaryComponent.tsx
+++ app/components/SummaryComponent.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { Chip, List, ListItem, ListItemText, Typography, Stack } from '@mui/material';
+import { Chip, List, ListItem, ListItemText, Typography, Stack, Avatar, Badge, Box, ListItemAvatar, Divider } from '@mui/material';
+import { Event } from '@mui/icons-material';
 import { makeSafeForCSS } from '@util/convert';
 import { highlightPlayerMouseEnterHandler, highlightPlayerMouseLeaveHandler } from "@components/highlightCharacter";
+import { Fragment } from 'react';
 
 interface SummaryProps {
     title: string;
@@ -26,21 +28,36 @@ export default function SummaryComponent (props:SummaryProps) {
     return( 
         <div className={`flex flex-col align-center w-full mx-2 mb-2 rounded rounded-b-none px-2 ${applyStyleClasses()}`}>
             <Typography align='center'>{props.title}</Typography>
+
             <List dense={true} disablePadding>
                 {props.summaries.map( (summary,i)=>
                     <ListItem key={i} dense={true} alignItems="flex-start" disablePadding>
                         <ListItemText
-                            primary={<Typography variant='body1' className={makeSafeForCSS(summary.name, 'player')} onMouseEnter={highlightPlayerMouseEnterHandler}
-                            onMouseLeave={highlightPlayerMouseLeaveHandler}>{summary.name}</Typography>}
-                            secondary={
-                                <Stack component='span' direction='column'>
-                                    <Typography variant='body2' component={'span'}>{summary.player?.id}</Typography>
-                                    <Stack component='span' direction='row' gap={1}>
-                                        <Chip component={'span'} size='small' variant="outlined" label={`Avg Rank ${(summary.ranks.reduce( (acc,cur) => acc + cur) / summary.count).toFixed(2)}`} />
-                                        <Chip component={'span'} size='small' variant="outlined" label={`Days ${summary.count}`} />
+                            primary={
+                                <Stack component='span' direction='row' justifyContent={'space-between'}>
+                                    <Stack direction='row' gap={1}>
+                                        <Avatar sx={{ width: 32, height: 32, mt: 1}} alt={summary.player.characters.find( c => c)?.profession ?? 'Any'} src={`icons/${summary.player.characters.find( c => c)?.profession ?? 'Any'}.png`} />
+                                        <Stack component='span' direction='column'>
+                                            <Typography variant='body2' className={makeSafeForCSS(summary.name, 'player')} 
+                                                onMouseEnter={highlightPlayerMouseEnterHandler}
+                                                onMouseLeave={highlightPlayerMouseLeaveHandler}>
+                                                    {summary.name}
+                                            </Typography>
+                                            <Typography variant='caption' component={'span'}>
+                                                {summary.player?.id}
+                                            </Typography>
+                                        </Stack>
                                     </Stack>
+                                </Stack>
+                            }
+                            secondary={
+                                <Stack component='span' direction='row' gap={1} justifyContent='space-between'  sx={{ pr: 1}}>
+                                    <Chip component={'span'} size='small' variant="outlined" label={`Avg Rank ${(summary.ranks.reduce( (acc,cur) => acc + cur) / summary.count).toFixed(2)}`} />
+                                    <Badge color="info" badgeContent={summary.count}>
+                                        <Event color='disabled'/>
+                                    </Badge>                                    
                                 </Stack>}
-                        />
+                        />  
                     </ListItem>
                 )}
             </List>
