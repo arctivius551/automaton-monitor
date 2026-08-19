7e4529c22d7e5bb082d3752a284aa08fe8a6bfd7
Ryan Bucinell <rbucinell.dev@gmail.com>
2026-08-19 10:00:59 -0400

    updating api route handler for ping

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,15 +1,18 @@
+import { NextResponse, type NextRequest } from 'next/server'
 import { run } from "@util/database";
-import type { NextApiRequest, NextApiResponse } from 'next'
- 
+
 type ResponseData = {
   message: string
 }
- 
-export async function GET( req: NextApiRequest, res: NextApiResponse<ResponseData> ) {
-    try{
-      await run();
-      return res.status(200).json({ message: 'Hello from Next.js!' });
-    }catch( e ){
-      return res.status(500).json({message: `${e}`});
-    }
+
+export async function GET(request: NextRequest) {
+  try{
+    //await run();
+    return NextResponse.json({message: 'Hello from Next.js'} as ResponseData);
+  }catch( e ){
+    return NextResponse.json( 
+      { message: `${e}`},
+      { status: 500 }
+    )
+  }
 }
